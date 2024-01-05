4159da489dbe5749c0e73a9004c726be3af3d24d
Sebastian Rodriguez <sebasrm2000@gmail.com>
2024-01-05 08:38:16 -0500

    style interactive back and front cards

--- src/components/BgCards/index.jsx
+++ src/components/BgCards/index.jsx
@@ -4,13 +4,14 @@ import bgCardBack from '../../assets/bg-card-back.png'
 
 function BgCards()  {
   return (
-        <div className='absolute top-[12%] left-[130px] flex flex-col gap-14'>
+        <div className='absolute top-[13%] left-[130px] flex flex-col gap-9 tracking-[3px] text-white'>
             <figure className='relative'>
-                <img src={bgCardFront} alt="" />
-                <div className='absolute bottom-0'>
-                    <p>0000 0000 0000 0000</p>
-                    <div className='flex'>
-                        <p> JANE NOSEQUE</p>
+            <svg className='absolute top-6 left-6' width="84" height="47" fill="none" xmlns="http://www.w3.org/2000/svg"><ellipse cx="23.478" cy="23.5" rx="23.478" ry="23.5" fill="#fff"/><path d="M83.5 23.5c0 5.565-4.507 10.075-10.065 10.075-5.559 0-10.065-4.51-10.065-10.075 0-5.565 4.506-10.075 10.065-10.075 5.558 0 10.065 4.51 10.065 10.075Z" stroke="#fff"/></svg>
+                <img className='' src={bgCardFront} alt="" />
+                <div className=' absolute bottom-0  w-[100%] text-left p-7'>
+                    <p className='text-[1.8rem] mb-5'>0000 0000 0000 0000</p>
+                    <div className='flex justify-between text-[0.9rem]'>
+                        <p> JANE APLESSED</p>
                         <div className='flex'>
                             <p>00</p>/
                             <p>00</p>
@@ -18,8 +19,11 @@ function BgCards()  {
                     </div>
                 </div>
             </figure>
-            <figure className='pl-[80px]'>
-                <img src={bgCardBack} alt="" />
+
+            
+            <figure className=' relative left-[95px]'>
+                <img className='' src={bgCardBack} alt="" />
+                <p className='absolute top-[110px] right-14'>000</p>
             </figure>
         </div>
 
